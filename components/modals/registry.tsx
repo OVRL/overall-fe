a8e3c2f1c1e03da757ab1a01b065f103a89e2a16
Jungsoo <sunpl13@naver.com>
2026-02-25 00:27:47 +0900

    feat: 모달 fallback 디자인 추가

--- components/modals/registry.tsx
+++ components/modals/registry.tsx
@@ -1,23 +1,29 @@
 import dynamic from "next/dynamic";
 import { ModalComponentMap } from "./types";
+import ModalLoadingFallback from "./ModalLoadingFallback";
 
 const DefaultImageModal = dynamic(() => import("./DefaultImageModal"), {
   ssr: false,
+  loading: () => <ModalLoadingFallback />,
 });
 const AddressSearchModal = dynamic(() => import("./AddressSearchModal"), {
   ssr: false,
+  loading: () => <ModalLoadingFallback />,
 });
 const EditProfileImageModal = dynamic(() => import("./EditProfileImageModal"), {
   ssr: false,
+  loading: () => <ModalLoadingFallback />,
 });
 const PlayerSearchModal = dynamic(
   () => import("./PlayerSearchModal/PlayerSearchModal"),
   {
     ssr: false,
+    loading: () => <ModalLoadingFallback />,
   },
 );
 const RegisterGameModal = dynamic(() => import("./RegisterGameModal"), {
   ssr: false,
+  loading: () => <ModalLoadingFallback />,
 });
 
 export const MODAL_REGISTRY: ModalComponentMap = {
